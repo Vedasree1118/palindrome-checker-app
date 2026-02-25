23b56aa4ea8510f4cfb8c638c8196893ba5baf58
aposcus <ritiknegimail@gmail.com>
2026-02-25 09:12:22 +0530

    [Name] Added usecaseName

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,5 +1,7 @@
 public class PalindromeCheckerApp {
     public static void main(String[]args){
-        System.out.println("Welcome to palindrome checker app");
+        System.out.println("Welcome to the Palindrome Checker Management System");
+        System.out.println("Version : 1.0");
+        system.out.println("System initialized successfully");
     }
 }
